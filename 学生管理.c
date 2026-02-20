c62a5976a833c8297b5c7d7ef5db593980525263
艾伦野鸽 <2431592580@qq.com>
2026-02-20 11:16:48 +0800

    第三次提交

--- 学生管理.c
+++ 学生管理.c
@@ -197,3 +197,193 @@ void freeAllList() {
     head->next = NULL;
 }
 
+//3. 文件操作
+void saveToBinaryFile() {
+    FILE *fp = fopen(DATA_FILE, "wb");
+    if (fp == NULL) {
+        printf("保存失败:无法打开文件%s\n", DATA_FILE);
+        return;
+    }
+
+    Student *p = head->next;
+    int cnt = 0;
+    while (p) {
+        int dataSize = sizeof(Student) - 2 * sizeof(struct Student*);
+        fwrite(p, dataSize, 1, fp);
+        p = p->next;
+        cnt++;
+    }
+    fclose(fp);
+    printf("成功保存 %d 条数据到文件\n", cnt);
+}
+
+//数据读取
+void loadFromBinaryFile() {
+    FILE *fp = fopen(DATA_FILE, "rb");
+    if (fp == NULL) {
+        return;
+    }
+
+    freeAllList();
+
+    Student temp;
+    int dataSize = sizeof(Student) - 2 * sizeof(struct Student*);
+
+    while (fread(&temp, dataSize, 1, fp)) {
+        Student *newNode = createNode();
+        //内存拷贝
+        memcpy(newNode, &temp, dataSize);
+        newNode->next = NULL;
+        newNode->prev = NULL;
+        //重新计算保证数据正确
+        calculateStats(newNode);
+        appendStudent(newNode);
+    }
+    fclose(fp);
+    printf("系统启动:已加载历史数据\n");
+}
+
+//4. 排序
+void swapNodeData(Student *a, Student *b) {
+    Student temp;
+    // 备份a的数据
+    strcpy(temp.id, a->id);
+    strcpy(temp.name, a->name);
+    strcpy(temp.gender, a->gender);
+    temp.age = a->age;
+    temp.score_c = a->score_c;
+    temp.score_math = a->score_math;
+    temp.score_eng = a->score_eng;
+    temp.total = a->total;
+    temp.average = a->average;
+
+    // a = b
+    strcpy(a->id, b->id);
+    strcpy(a->name, b->name);
+    strcpy(a->gender, b->gender);
+    a->age = b->age;
+    a->score_c = b->score_c;
+    a->score_math = b->score_math;
+    a->score_eng = b->score_eng;
+    a->total = b->total;
+    a->average = b->average;
+
+    // b = temp
+    strcpy(b->id, temp.id);
+    strcpy(b->name, temp.name);
+    strcpy(b->gender, temp.gender);
+    b->age = temp.age;
+    b->score_c = temp.score_c;
+    b->score_math = temp.score_math;
+    b->score_eng = temp.score_eng;
+    b->total = temp.total;
+    b->average = temp.average;
+}
+
+//冒泡排序
+//sortType 1 = 总分 2 = c语言 3 = 数学 4 = 英语
+//order 1 = 升序 2 = 降序
+void sortStudents(int sortType, int order) {
+    if (head->next == NULL) return;
+
+    int swapped;
+    Student *ptr1;
+    Student *lptr = NULL;
+
+    do {
+        swapped = 0;
+        ptr1 = head->next;
+
+        while (ptr1->next != lptr) {
+            int condition = 0;
+            float val1, val2;
+
+            switch (sortType) {
+                case 1: val1 = ptr1->total; val2 = ptr1->next->total; break;
+                case 2: val1 = ptr1->score_c; val2 = ptr1->next->score_c; break;
+                case 3: val1 = ptr1->score_math; val2 = ptr1->next->score_math; break;
+                case 4: val1 = ptr1->score_eng; val2 = ptr1->next->score_eng; break;
+            }
+
+            if (order == 1) condition = (val1 > val2); //升序(不符合升序的情况condition == 1)
+            else condition = (val1 < val2); //降序
+
+            if (condition) {
+                swapNodeData(ptr1, ptr1->next);
+                swapped = 1;
+            }
+            ptr1 = ptr1->next;
+        }
+        lptr = ptr1;
+    } while (swapped);
+
+    printf("排序完成\n");
+}
+
+//排序菜单
+void menuSort() {
+    int type, order;
+    printf("\n=== 排序选项 ===\n");
+    printf("1.按总分\n2. 按C语言\n3. 按数学\n4. 按英语\n");
+    type = readInt("请选择排序依据(1-4):", 1, 4);
+
+    printf("1. 升序\n2. 降序\n");
+    order = readInt("请选择排序方式(1-2):", 1, 2);
+
+    sortStudents(type, order);
+    pauseStytem();
+}
+
+
+
+
+
+
+
+
+
+
+int main() {
+    // 1. 初始化系统
+    initList();
+       
+    // 2. 加载数据
+    loadFromBinaryFile();
+
+    int mainChoice;
+    while (1) {
+        printf("\n########################################\n");
+        printf("#       学生信息管理系统 V2.1      #\n");
+        printf("########################################\n");
+        printf("1. 登录系统\n");
+        printf("2. 注册账号 (默认注册为学生)\n");
+        printf("0. 退出程序\n");
+        
+        mainChoice = readInt("请选择操作: ", 0, 2);
+
+        if (mainChoice == 0) {
+            printf("正在保存数据并退出...\n");
+            saveToBinaryFile();
+            freeAllStudents();
+            free(head);
+            printf("再见\n");
+            break;
+        }
+        else if (mainChoice == 2) {
+            
+        }
+        else if (mainChoice == 1) {
+            if (performLogin()) {
+                printf("登录成功！欢迎, %s\n", currentUser.username);
+                // 根据权限分发
+                if (currentUser.role == 1) 
+                else if (currentUser.role == 2) 
+                else if (currentUser.role == 3) 
+            } else {
+                printf("登录失败：账号或密码错误。\n");
+            }
+        }
+    }
+
+    return 0;
+}
